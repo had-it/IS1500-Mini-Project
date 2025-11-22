d40a69688dfe892ce226dc4e309da863dd4e97d7
hpnpvvf4zt-lgtm <hpnpvvf4zt@privaterelay.appleid.com>
2025-11-22 17:46:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -43,6 +43,18 @@ int main(){
 
 // Switches and buttons
 
+// Buttons
+int get_btn(void) {
+  volatile int* button = (volatile int*) 0x040000d0; // The button is mapped to memory address 0x040000d0.
+  return (*button) & 1; // Return current status of the push-button (which lies in lsb)
+}
+
+// Swtiches
+int get_sw( void ){
+  volatile int* switches = (volatile int*) 0x04000010;  //  The toggles (or switches) are mapped to memory address 0x04000010
+  return (*switches) & 0x3FF; // Checks the status of the switches in the 10 lsbs
+}
+
 
 
 
@@ -50,9 +62,49 @@ int main(){
 
 int fractal_type = 0; // 0 = Mandelbrot, 1 = Burning Ship
 
+
+
+// Labinit and timeinterrupt
+// Code from Lab3
+
+/* Below is the function that will be called when an interrupt is triggered. */
+void handle_interrupt(unsigned cause) 
+{
+    volatile unsigned int* T_Stat = (volatile unsigned int*) 0x04000020;
+    *T_Stat = 0;     // Clear TO bit, resetting the status
+    timeoutcount++;
+
+    if(timeoutcount == 10){
+        timeoutcount = 0; 
+        // set_display with first argument being the display and second being the int do be displayed
+        for(int i = 1; i<= 6; i++){
+            set_displays(i, get_time_digit(mytime, i));
+        }
+        tick( &mytime ); // Ticks the clock once
+    }
+    *(T_Ctrl) = 0x5;
+}
+
+/* Add your code here for initializing interrupts. */ // Local interupts
+void labinit(void)
+{
+    
+  int period = ((30000000/10) - 1); // 10 ms (-1 because count to 0)
+  volatile unsigned short* T_periodLo = (unsigned short*) 0x04000028;
+  volatile unsigned short* T_periodHi = (unsigned short*) 0x0400002c;
+
+  *(T_periodLo) = period & 0xFFFF; // (16 LSB)
+  *(T_periodHi) = period >> 16; // (shifts 16 bits to the left = 16 MSB)
+  
+  *(T_Ctrl) = 0x5; // Start the timer and ITO=1
+  asm volatile ("csrsi mie,16"); // machine interrupt enable control register. Accept interrupts from Timer
+  asm volatile ("csrsi mstatus,3"); // mstatus = machine status control register. Enabe interrupts
+}
+
 /* ------ MAIN ------- */
 
 main(){
+    labinit();
 
     // SET UP VGA
 
@@ -62,9 +114,6 @@ main(){
     fractals(fractal_type); // Mandelbrot
 
     while(1){
-        labinit(); // infinite zoom with timeinterrupts
-
-        bool time_interrup = false;
 
         // Switch: color palette
             // time interrupt
