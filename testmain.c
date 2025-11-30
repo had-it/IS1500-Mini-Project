1d420be1fb84132f483e813ed7a7fae2ef199615
Hadia Abdulova <abdulova@kth.se>
2025-11-30 15:58:28 +0100

    Debounce after break

--- testmain.c
+++ testmain.c
@@ -91,12 +91,14 @@ int main(void) {
         if (sw & (1u << 0) && ((btn & BUTTON_DRAW_MASK) && !(last_btn & BUTTON_DRAW_MASK))) {
             build_palette(palette1, 0); // Palette 1
             palette = palette1;
+            asm_pause(200000);
             break; // Exit loop after selecting palette
         }
         // Switch 1
         if (sw & (1u << 1) && ((btn & BUTTON_DRAW_MASK) && !(last_btn & BUTTON_DRAW_MASK))) {
             build_palette(palette2, 1); // Palette 2
             palette = palette2;
+            asm_pause(200000);
             break; // Exit loop after selecting palette
         }    
 
@@ -116,12 +118,14 @@ int main(void) {
         if (sw & (1u << 0) && ((btn & BUTTON_DRAW_MASK) && !(last_btn & BUTTON_DRAW_MASK)))  { // If switch 0 is on and button is pressed
             fractal_type = 0; // Mandelbrot
             draw_fractal_to_fb(fractal_type, palette, scale, center_x, center_y);
+            asm_pause(200000);
             break; // Exit loop after drawing
         }
         // Switch 1
         if (sw & (1u << 1) && ((btn & BUTTON_DRAW_MASK) && !(last_btn & BUTTON_DRAW_MASK))) { // If switch 1 is on and button is pressed
             fractal_type = 1; // Burning Ship
             draw_fractal_to_fb(fractal_type, palette, scale, center_x, center_y);
+            asm_pause(200000);
             break; // Exit loop after drawing
         }
 
